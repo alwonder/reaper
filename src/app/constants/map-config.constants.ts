1300a38340d904ef3b361501568c94f81dbfdbc5
Sergey Eliseev <thefury014@gmail.com>
2022-01-30 22:00:25 +0900

    Add max zoom for tile layer

--- src/app/constants/map-config.constants.ts
+++ src/app/constants/map-config.constants.ts
@@ -9,6 +9,7 @@ export const mapStyle: Style = {
         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
       ],
       'tileSize': 256,
+      maxzoom: 17,
       // 'attribution':
       //   'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
     }
